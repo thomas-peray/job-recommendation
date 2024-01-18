8ff2f6847b04f5a4a11aba0d3e4b680adea4e0bb
Thomas Peray <thomas.peray5@gmail.com>
2024-01-18 18:36:05 +0200

    Remove duplicated function

--- job_recommender.py
+++ job_recommender.py
@@ -86,10 +86,3 @@ def ngrams(string, n=3):
     string = re.sub(r'[,-./]|\sBD', r'', string)
     ngrams = zip(*[string[i:] for i in range(n)])
     return [''.join(ngram) for ngram in ngrams]
-
-
-
-def getNearestN(query):
-    queryTFIDF_ = vectorizer.transform(query)
-    distances, indices = nbrs.kneighbors(queryTFIDF_)
-    return distances, indices
