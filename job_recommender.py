3633251f6767d4464dd4cd49a0f68c60327fd17d
E21A443E <gregoire.cosne@etu.univ-nantes.fr>
2024-01-18 17:45:45 +0200

    update of job_recommender.py

--- job_recommender.py
+++ job_recommender.py
@@ -22,22 +22,21 @@ with open(file_path, 'r') as file:
 experience = re.search(r'Experience : "(\d+)', resume_content).group(1)
 qualifications = re.search(r'Qualifications\s*:\s*"([^"]*)"', resume_content).group(1)
 salary_range = re.search(r'Salary Range\s*:\s*"([^"]*)"', resume_content).group(1)
-location = re.search(r'location\s*:\s*"([^"]*)"', resume_content).group(1)
-country = re.search(r'Country\s*:\s*"([^"]*)"', resume_content).group(1)
-work_type = re.search(r'Work Type\s*:\s*"([^"]*)"', resume_content).group(1)
+skills = re.search(r'Skills\s*:\s*"([^"]*)"', resume_content).group(1)
 
 # Create a dictionary to represent the resume information
 resume_info = {
     'Experience': experience,
     'Qualifications': qualifications,
     'Salary Range': salary_range,
-    'location': location,
-    'Country': country,
-    'Work Type': work_type,
+    'Skills': skills,
 }
 
 # Convert the dictionary to a DataFrame
 resume_df = pd.DataFrame([resume_info])
+
+# Combine the extracted information into a single string:
+resume_text = f"{experience} {qualifications} {salary_range} {skills}"
 
 
 def ngrams(string, n=3):
@@ -50,7 +49,7 @@ def ngrams(string, n=3):
     string = string.replace('&', 'and')
     string = string.replace(',', ' ')
     string = string.replace('-', ' ')
-    string = string.title()  # normalize case - capital at start of each word
+    string = string.title()  # normalize case - capital at the start of each word
     string = re.sub(' +', ' ', string).strip()  # get rid of multiple spaces and replace with a single
     string = ' ' + string + ' '  # pad names for ngrams...
     string = re.sub(r'[,-./]|\sBD', r'', string)
@@ -59,10 +58,10 @@ def ngrams(string, n=3):
 
 
 vectorizer = TfidfVectorizer(min_df=1, analyzer=ngrams, lowercase=False)
-tfidf = vectorizer.fit_transform(resume_df['Qualifications'].astype('U'))
+tfidf = vectorizer.fit_transform([resume_text])
 
 nbrs = NearestNeighbors(n_neighbors=1, n_jobs=-1).fit(tfidf)
-jd_test = jd_df['Qualifications'].astype('U').values
+jd_test = jd_df['Job Description'].astype('U').values
 
 
 def getNearestN(query):
@@ -70,6 +69,15 @@ def getNearestN(query):
     distances, indices = nbrs.kneighbors(queryTFIDF_)
     return distances, indices
 
+
+distances, indices = getNearestN(jd_test)
+test = list(jd_test)
+matches = []
+
+for i, j in enumerate(indices):
+    dist = round(distances[i][0], 2)
+    temp = [dist]
+    matches.append(temp)
 
 distances, indices = getNearestN(jd_test)
 matches_df = pd.DataFrame({'Match confidence': distances.flatten()})
