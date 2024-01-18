31ef44461a47f3496fa7a247f05e1d40827d1c68
Thomas Peray <thomas.peray5@gmail.com>
2024-01-18 15:57:21 +0200

    User interface

--- main.py
+++ main.py
@@ -1,24 +1,6 @@
-import tkinter as tk
-import customtkinter as ctk
 import data_processing as dp
-
-def clear_frame(frame):
-   for widgets in frame.winfo_children():
-      widgets.destroy()
-
-
-ctk.set_appearance_mode("system")
-ctk.set_default_color_theme("dark-blue")
-
-window = ctk.CTk()
-window.geometry("1000x600")
-window.title("Job recommendation")
-
-mainframe = ctk.CTkFrame(window)
-mainframe.pack(fill="both", expand=True)
+import user_interface as ui
 
 evidence, labels = dp.load_data("job_profiles.csv")
 
-
-window.mainloop()
-
+ui.launch_interface()
